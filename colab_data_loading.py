a740cfcadf3f0595b83608dc5c6ec33910415d0f
Michael Wolf <mjwolf@umich.edu>
2020-04-21 16:24:23 -0400

    merge origin/data_augmentation into master

--- colab_data_loading.py
+++ colab_data_loading.py
@@ -1,39 +1,30 @@
-from scipy.io import loadmat
 import os
+import numpy as np
+import json
+from detectron2.structures import BoxMode
+from scipy.io import loadmat
 
 def get_data(d):
-    """
-    if your dataset is in COCO format, this cell can be replaced by the following three lines:
-    from detectron2.data.datasets import register_coco_instances
-    register_coco_instances("my_dataset_train", {}, "json_annotation_train.json", "path/to/image/dir")
-    register_coco_instances("my_dataset_val", {}, "json_annotation_val.json", "path/to/image/dir")
-    """
     dataset_list = []
 
     # TODO: change annotation in the next line to the local directory of your annotation data set
-    for root,dir,files in os.walk('Obstacle-Detection-442/data/annotationsV2_rectified_'+d):
+    for root,dir,files in os.walk('./annotationsV2_rectified'):
         if len(root)>64:
             # record is a dict contains info of 1 training image
             record = {}
             annot_dir_path = root
-            dir_name_parsed = annot_dir_path.split('/', 5)[3]
+            dir_name_parsed = annot_dir_path.split('/', 3)[2]
             for file_name in files:
                 file_name_parsed = file_name.split('.')[0][:-1]
 
                 # data file path
                 # TODO: change image_data to the local directory of your image dataset
-                full_image_path = 'Obstacle-Detection-442/data/video_data' + '/'+ str(dir_name_parsed) + '/'+'framesRectified'+ '/'+file_name_parsed + 'L.jpg'
+                full_image_path = './video_data' + '/'+ str(dir_name_parsed) + '/'+'framesRectified'+ '/'+file_name_parsed + 'L.jpg'
                 record["file_name"] = full_image_path
 
                 # data file name
                 image_file_name = file_name_parsed + 'L.jpg'
                 record["image_id"] = image_file_name
-
-                # data image height (all of modd2 has image size of 1278x958)
-                record["height"] = 958
-
-                # data image width (all of modd2 has image size of 1278x958)
-                record["width"] = 1278
 
                 # process annotation
                 full_path = annot_dir_path + '/' + str(file_name)
@@ -46,25 +37,15 @@ def get_data(d):
                 if obstacle.shape[0] > 0:
                     for i in range(obstacle.shape[0]):
                         bbox = obstacle[i,:].tolist()
-                        bbox_mode = BoxMode.XYWH_ABS
+                        bbox_mode =BoxMode.XYWH_ABS
 
                         # annotation_instance is a dict that contains info of one instance in one training image
-                        #   
-                        # removed <"segmentation":sea_edge,> because coco interprets as a mask for true segmentation
-                        # rather than a line labelling the water's edge
-                        annotation_instance = {"bbox":bbox, "bbox_mode":bbox_mode, "category_id":0}
+                        annotation_instance = {"bbox":bbox, "bbox_mode":bbox_mode,"segmentation":sea_edge,"category_id":0}
                         annotation_list.append(annotation_instance)
 
                 record["annotations"] = annotation_list
 
                 # adding entry to the dataset dictionary
                 dataset_list.append(record)
-    print(dataset_list[10]['annotations'])
+
     return dataset_list
-
-
-def main():
-    get_data()
-
-if __name__ == "__main__":
-    main()
---
+++ data_aug_translation.py
@@ -0,0 +1,79 @@
+import numpy as np
+from scipy.io import loadmat
+import cv2
+import os
+import random
+
+def data_aug():
+    # make augmentation folder
+    if not os.path.exists("./data_aug_shift_annotation"):
+        os.makedirs("./data_aug_shift_annotation")
+    if not os.path.exists("./data_aug_shift_image_data"):
+        os.makedirs("./data_aug_shift_image_data")
+    if not os.path.exists("./data_aug_shift_annotation/annotationsV2_rectified"):
+        os.makedirs("./data_aug_shift_annotation/annotationsV2_rectified")
+    for root,dir,files in os.walk('./annotation/annotationsV2_rectified'):
+        data_aug_annot_dir_prefix = "./data_aug_shift_annotation/annotationsV2_rectified\\"
+        if len(root)>64:
+            annot_dir_path = root
+            dir_name_parsed = annot_dir_path.split("\\")[1]
+            augmented_annotaton_dir = data_aug_annot_dir_prefix + dir_name_parsed + "\\ground_truth"
+
+            for file_name in files:
+                file_name_parsed = file_name.split('.')[0][:-1]
+                original_image_path = './image_data/video_data' + '/'+ str(dir_name_parsed) + '/'+'frames'+ '/'+file_name_parsed + 'L.jpg'
+                augmented_image_path = './data_aug_shift_image_data/video_data' + '/'+ str(dir_name_parsed) + '/'+'frames'+ '/'+file_name_parsed + 'L.jpg'
+
+                if not os.path.exists('./data_aug_shift_image_data/video_data' + '/'+ str(dir_name_parsed) + '/'+'frames'):
+                    os.makedirs('./data_aug_shift_image_data/video_data' + '/'+ str(dir_name_parsed) + '/'+'frames')
+
+                augmented_annotation_path = augmented_annotaton_dir + '/' + str(file_name.split('.')[0][:])
+                if not os.path.exists(augmented_annotaton_dir):
+                    os.makedirs(augmented_annotaton_dir)
+
+                img = cv2.imread(original_image_path)
+
+                original_annotation_path = annot_dir_path + '/' + str(file_name)
+                original_annotation = loadmat(original_annotation_path)['annotations']
+                obstacle = original_annotation['obstacles'][0, 0]
+
+                # only make changes to the images that have at least 1 obstacle
+                if obstacle.shape[0] > 0:
+                    img,obstacle = translation(img,obstacle)
+
+                np.save(augmented_annotation_path,obstacle)
+                write_status = cv2.imwrite(augmented_image_path, img)
+                # check if writing image is successful
+                # print(write_status)
+    return None
+
+
+def translation(img,obstacle):
+    row_shift = random.randint(-100,100)
+    col_shift = random.randint(-100,100)
+
+    for i in range(obstacle.shape[0]):
+        col_cor = obstacle[i][1]
+        row_cor = obstacle[i][0]
+        w = obstacle[i][2]
+        h = obstacle[i][3]
+        # check boundary condition
+        if 0 < col_cor + col_shift < img.shape[1] and 0 < row_cor + row_shift <img.shape[0]:
+            if col_cor + col_shift + w < img.shape[1] and row_cor + row_shift + h < img.shape[1]:
+                bbox_entry = np.array([col_cor + col_shift, row_cor + row_shift, w, h])
+                if i==0:
+                    bbox = bbox_entry
+                else:
+                    np.vstack((bbox,bbox_entry))
+        else :
+            bbox = obstacle
+
+    img = np.roll(img, (row_shift, col_shift), axis=(0, 1))
+    return img,bbox
+
+def main():
+    
+    data_aug()
+
+if __name__ == "__main__":
+    main()

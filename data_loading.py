16f4f3622d998c0ac494ddc102ce2aacd09a8896
mrako <mrako@umich.edu>
2020-04-23 18:30:40 -0400

    Changed data_loading.py to match the Colab notebook
    Forgot to include this update in last commit

--- data_loading.py
+++ data_loading.py
@@ -1,23 +1,31 @@
-from scipy.io import loadmat
+# Always run this file after grabbing the data from git. It inits the data
+# loading functions. I left in the original example balloon function and loader
+# to compare against if we ever need to debug
+
+# if your dataset is in COCO format, this cell can be replaced by the following three lines:
+# from detectron2.data.datasets import register_coco_instances
+# register_coco_instances("my_dataset_train", {}, "json_annotation_train.json", "path/to/image/dir")
+# register_coco_instances("my_dataset_val", {}, "json_annotation_val.json", "path/to/image/dir")
+
 import os
+import numpy as np
+import json
+from detectron2.structures import BoxMode
+from scipy.io import loadmat
 
 def get_data(d):
-    """
-    if your dataset is in COCO format, this cell can be replaced by the following three lines:
-    from detectron2.data.datasets import register_coco_instances
-    register_coco_instances("my_dataset_train", {}, "json_annotation_train.json", "path/to/image/dir")
-    register_coco_instances("my_dataset_val", {}, "json_annotation_val.json", "path/to/image/dir")
-    """
     dataset_list = []
 
     # TODO: change annotation in the next line to the local directory of your annotation data set
     for root,dir,files in os.walk('Obstacle-Detection-442/data/annotationsV2_rectified_'+d):
         if len(root)>64:
-            # record is a dict contains info of 1 training image
-            record = {}
+            
             annot_dir_path = root
             dir_name_parsed = annot_dir_path.split('/', 5)[3]
+            
             for file_name in files:
+                # record is a dict contains info of 1 training image
+                record = {}
                 file_name_parsed = file_name.split('.')[0][:-1]
 
                 # data file path
@@ -52,19 +60,28 @@ def get_data(d):
                         #   
                         # removed <"segmentation":sea_edge,> because coco interprets as a mask for true segmentation
                         # rather than a line labelling the water's edge
-                        annotation_instance = {"bbox":bbox, "bbox_mode":bbox_mode, "category_id":0}
+                        annotation_instance = {
+                            "bbox":bbox, 
+                            "bbox_mode":bbox_mode, 
+                            "category_id":0,
+                            "iscrowd":0
+                        }
                         annotation_list.append(annotation_instance)
 
                 record["annotations"] = annotation_list
 
                 # adding entry to the dataset dictionary
                 dataset_list.append(record)
-    print(dataset_list[10]['annotations'])
+
     return dataset_list
 
 
-def main():
-    get_data()
 
-if __name__ == "__main__":
-    main()
+from detectron2.data import DatasetCatalog, MetadataCatalog
+DatasetCatalog.clear()
+for d in ["train", "val", "test"]:
+    if(("modd2_" + d) not in DatasetCatalog.list()):
+        DatasetCatalog.register("modd2_" + d, lambda d=d: get_data(d))
+    MetadataCatalog.get("modd2_" + d).set(thing_classes=["obstacle"])
+modd2_metadata = MetadataCatalog.get("modd2_train")
+print("Metadata Registered Successfully")
